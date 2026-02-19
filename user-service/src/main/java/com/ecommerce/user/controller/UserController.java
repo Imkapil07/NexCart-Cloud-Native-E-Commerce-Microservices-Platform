d6d4e427428a37e049c2279716a5621037eb5f66
Imkapil07 <kapilraivines@gmail.com>
2026-02-19 12:27:09 +0530

    add kafka fiel

--- user-service/src/main/java/com/ecommerce/user/controller/UserController.java
+++ user-service/src/main/java/com/ecommerce/user/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+
 /**
  * REST controller for user registration, login,
  * and profile retrieval. Base path: /api/users
